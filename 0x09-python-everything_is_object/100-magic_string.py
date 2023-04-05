12bb899e5f7c7af646a5cf596b807a128b79e2b9
emmanuelofor <emmanuel.ofori@kibo.school>
2023-04-04 17:28:35 -0700

    Task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, 'count'):
-        magic_string.count = 0
-    magic_string.count += 1
-    return ', '.join(['BestSchool' for _ in range(magic_string.count)])
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
