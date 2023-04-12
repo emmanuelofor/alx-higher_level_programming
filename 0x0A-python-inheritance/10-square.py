ce8ae1bc85d815ce5d21e18011474eeedc4e2a32
emmanuelofor <emmanuel.ofori@kibo.school>
2023-04-11 22:11:02 -0700

    All tasks completed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""a Rectangle subclass Square is defined"""
+"""A Rectangle subclass Square is defined"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square"""
+    """A square is represented"""
 
     def __init__(self, size):
-        """a new square is initialized
+        """A new square is initialized
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = 
+        self.__size = size
