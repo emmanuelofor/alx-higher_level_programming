77cf5c0dfde591d1e7f37024b5e7a40d71dcfdcb
emmanuelofor <emmanuel.ofori@kibo.school>
2023-04-12 11:48:37 -0700

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A JSON file-writing function is written by this module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """An object to a text file using JSON format is written by this funciton"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
