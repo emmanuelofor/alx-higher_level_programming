819e04b3e4bd6339b48ad98826ad537f3712aa69
emmanuelofor <emmanuel.ofori@kibo.school>
2023-07-25 08:31:38 -0700

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    const todos = JSON.parse(body);
-    const completedTasks = {};
-    for (let i = 0; i < todos.length; i++) {
-      const todo = todos[i];
-      if (todo.completed) {
-        if (completedTasks.hasOwnProperty(todo.userId)) {
-          completedTasks[todo.userId] += 1;
-        } else {
-          completedTasks[todo.userId] = 1;
-        }
+  if (error) console.log(error);
+  else if (response.statusCode === 200) {
+    const dict = {};
+    for (let i = 0; i < JSON.parse(body).length; i++) {
+      const user = JSON.parse(body)[i];
+      if (user.userId in dict && user.completed === true) {
+        dict[user.userId] += 1;
+      } else if (!(user.userId in dict) && user.completed === true) {
+        dict[user.userId] = 1;
       }
     }
-    for (const userId in completedTasks) {
-      if (completedTasks[userId] === 0) {
-        delete completedTasks[userId];
-      }
-    }
-    console.log(completedTasks);
+    console.log(dict);
   }
 });
